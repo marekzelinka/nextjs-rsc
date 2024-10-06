4e15ed7835287bddc936494d5210b8de89bf9c48
Marek Zelinka <mzelinka17@gmail.com>
2024-10-06 18:03:39 +0200

    debounce search

--- src/app/components/search-input.tsx
+++ src/app/components/search-input.tsx
@@ -3,17 +3,28 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
+import { useDebouncedCallback } from "use-debounce";
 import { Spinner } from "./spinner";
 
 export function SearchInput({ search }: { search?: string }) {
   const { push } = useRouter();
 
-  const [isPending, startTransition] = useTransition();
+  const [isSearching, startTransition] = useTransition();
+
+  const handleSearch = useDebouncedCallback((search: string) => {
+    startTransition(() => {
+      if (search) {
+        push(`/?search=${search}`);
+      } else {
+        push("/");
+      }
+    });
+  }, 500);
 
   return (
     <div className="relative">
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        {isPending ? (
+        {isSearching ? (
           <Spinner className="size-5 animate-spin text-gray-400" />
         ) : (
           <MagnifyingGlassIcon className="size-5 text-gray-400" />
@@ -24,15 +35,7 @@ export function SearchInput({ search }: { search?: string }) {
         name="search"
         id="search"
         defaultValue={search}
-        onChange={(event) => {
-          startTransition(() => {
-            if (event.target.value) {
-              push(`/?search=${event.target.value}`);
-            } else {
-              push("/");
-            }
-          });
-        }}
+        onChange={(event) => handleSearch(event.target.value)}
         className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
         placeholder="Search"
         aria-label="Search users"
